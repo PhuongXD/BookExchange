2f0882fc1b1f8082e8d5e2f5bad60cb3b2889e1c
PhuongXD <lanphuong2899@gmail.com>
2019-12-15 20:48:13 +0700

    lailannua

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/UpdateBookForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class UpdateBookForm : Form
+    {
+        private int BookId;
+        private LogicLayer Business;
+        public UpdateBookForm(int id)
+        {
+            InitializeComponent();
+            this.BookId = id;
+            this.Business = new LogicLayer();
+            this.Load += UpdateBookForm_Load;
+            this.btnSave.Click += btnSave_Click;
+        }
+
+        void btnSave_Click(object sender, EventArgs e)
+        {
+            var namebook = this.txtNamebook.Text;
+            var author = this.txtAuthor.Text;
+            var topic = this.txtTopic.Text;
+            var type = this.txtType.Text;
+            //this.Business.UpdateBook(this.BookId, namebook, author, topic, type);
+            MessageBox.Show("Update student successfuly");
+            this.Close();
+
+        }
+
+        void UpdateBookForm_Load(object sender, EventArgs e)
+        {
+            var book = this.Business.GetBook(this.BookId);
+            this.txtNamebook.Text = book.NameBook;
+            this.txtAuthor.Text = book.Author;
+            this.txtTopic.Text = book.Topic;
+            this.txtType.Text = book.TypeExchange;
+            
+        }
+        
+
+       
+    }
+}
